d16c54e2c2fddc009a89266f6a21e7df95b3cd31
Pumpon4yk <tt10392@gmail.com>
2023-06-23 19:06:04 +0200

    styles date picker fix and add CalendarPage

---
+++ src/components/Calendar/Calendar.js
@@ -0,0 +1,12 @@
+import { CalendarToolbar } from 'components/CalendarToolbar/CalendarToolbar'
+import {Container} from './Calendar.styled'
+
+
+export const Calendar = () => {
+
+  return(
+    <Container>
+      <CalendarToolbar/>
+    </Container>
+  )
+}
