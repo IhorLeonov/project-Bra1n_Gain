427804619641373152de330656ab2d62b74f2812
333Nikita333 <nikitosfox333@gmail.com>
2023-06-23 14:46:17 +0300

    added partial slider code

---
+++ src/components/ReviewSlider/ReviewSlider.js
@@ -0,0 +1,80 @@
+import { useRef } from 'react';
+import { FaStar } from 'react-icons/fa';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import {
+  Avatar,
+  ButtonBox,
+  ButtonNext,
+  ButtonPreview,
+  GrayStar,
+  ReviewContainer,
+  ReviewInfo,
+  ReviewText,
+  ReviewTitle,
+  StarRating,
+  UserName,
+} from './ReviewSlider.styled';
+
+// временный файлик с отзывами. В будущем будут приходить данные с бека
+// и через useEffect нужно будет отрисовывать
+import reviewsData from './reviews.json';
+
+//нужно будет выполнить команду "npm install react-slick slick-carousel" для установки себе слайдера
+export const ReviewSlider = () => {
+  const sliderRef = useRef(null);
+
+  //настройки к слайдеру
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplay: true,  // свойство, которое автоматически листает слайды. Оставил закоментированным, чтобы не маячило и спокойно стилизировать
+    speed: 500,
+  };
+
+  //кнопка - предыдущий комент
+  const goToPreviousSlide = () => {
+    sliderRef.current.slickPrev();
+  };
+
+  //кнопка - слудющий комент
+  const goToNextSlide = () => {
+    sliderRef.current.slickNext();
+  };
+
+  // рендерит звезды (если оценка 4: делает 4 звезды желтыми и 1 серую)
+  const renderStars = rating => {
+    const stars = [];
+    for (let i = 0; i < 5; i++) {
+      if (i < rating) {
+        stars.push(<FaStar key={i} />);
+      } else {
+        stars.push(<GrayStar key={i} />);
+      }
+    }
+    return <StarRating>{stars}</StarRating>;
+  };
+
+  return (
+    <ReviewContainer>
+      <ReviewTitle>Reviews</ReviewTitle>
+      <ReviewInfo ref={sliderRef} {...settings}>
+        {reviewsData.map((review, index) => (
+          <div key={index}>
+            <Avatar src={review.avatar} alt={review.name} />
+            <UserName>{review.name}</UserName>
+            {renderStars(parseInt(review.rate, 10))}
+
+            <ReviewText>{review.comment}</ReviewText>
+          </div>
+        ))}
+      </ReviewInfo>
+      <ButtonBox>
+        <ButtonPreview onClick={goToPreviousSlide}>Previous</ButtonPreview>
+        <ButtonNext onClick={goToNextSlide}>Next</ButtonNext>
+      </ButtonBox>
+    </ReviewContainer>
+  );
+};
--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -7,24 +7,28 @@ import {
   SignUp,
   Login,
 } from './HomePage.styled.jsx';
+import { ReviewSlider } from 'components/ReviewSlider/ReviewSlider.js';
 
 const Home = () => {
   return (
-    <Header>
-      <Wrapper>
-        <MainTitle>
-          G<OoTxt>oo</OoTxt>seTrack
-        </MainTitle>
-        <BtnContainer>
-          <SignUp to={'/register'} type="button">
-            Sign up
-          </SignUp>
-          <Login to={'/login'} type="button">
-            Log in
-          </Login>
-        </BtnContainer>
-      </Wrapper>
-    </Header>
+    <>
+      <Header>
+        <Wrapper>
+          <MainTitle>
+            G<OoTxt>oo</OoTxt>seTrack
+          </MainTitle>
+          <BtnContainer>
+            <SignUp to={'/register'} type="button">
+              Sign up
+            </SignUp>
+            <Login to={'/login'} type="button">
+              Log in
+            </Login>
+          </BtnContainer>
+        </Wrapper>
+      </Header>
+      <ReviewSlider />
+    </>
   );
 };
 export default Home;
