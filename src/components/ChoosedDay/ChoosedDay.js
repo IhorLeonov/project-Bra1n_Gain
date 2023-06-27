6ad15c35964ad3f7b421264fcc4aaf05716979fb
Ruslan Karpov <rkarpov88@gmail.com>
2023-06-27 14:43:34 +0300

    refresh yesterday fix

--- src/components/ChoosedDay/ChoosedDay.js
+++ src/components/ChoosedDay/ChoosedDay.js
@@ -1,23 +1,27 @@
 import { useDispatch, useSelector } from 'react-redux';
 
-
 import { CalendarHead } from '../CalendarHead/CalendarHead';
 import { TasksColumnsList } from './TasksColumnsList';
 
 import { getDate } from 'redux/currentDate/selector';
+import { setDate } from 'redux/currentDate/curentDateSlice';
 import { useEffect } from 'react';
 import { fetchAllTasks } from 'redux/task/operations';
 
 const ChoosedDay = () => {
   const dispatch = useDispatch();
   const date = new Date(useSelector(getDate));
+
+  const handleSetDate = newDate => {
+    dispatch(setDate(newDate.toString()));
+  };
   useEffect(() => {
     dispatch(fetchAllTasks());
   }, [dispatch]);
 
   return (
     <>
-      <CalendarHead date={date} setDate={handleSetDate}/>
+      <CalendarHead date={date} setDate={handleSetDate} />
       <TasksColumnsList date={date} />
     </>
   );
