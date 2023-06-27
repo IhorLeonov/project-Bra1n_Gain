6df73f1781d4506a9679055d9bea30910e99b62e
Ihor Leonov <ihorleonow@gmail.com>
2023-06-27 17:59:57 +0300

    small fix

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,180 +1,177 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from 'react';
+// import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker.css';
 import { Formik } from 'formik';
 import { updateUser } from 'redux/auth/operations';
-
 
 import { selectUser } from 'redux/auth/selectors.js';
 
 import {
-    Wrapper,
-    Avatar,
-    AvatarWrapper,
-    FormUserProfile,
-    BtnUploadAvatar,
-    LabelAvatar,
-    Input,
-    Label,
-    SubmitBtn,
-    UserLabel,
-    UserName,
-    DatePickerStyles,
-    DatePickerWrapper,
-    MainFieldWrapper,
-    BlockFieldWrapper,
+  Wrapper,
+  Avatar,
+  AvatarWrapper,
+  FormUserProfile,
+  BtnUploadAvatar,
+  LabelAvatar,
+  Input,
+  Label,
+  SubmitBtn,
+  UserLabel,
+  UserName,
+  DatePickerStyles,
+  DatePickerWrapper,
+  MainFieldWrapper,
+  BlockFieldWrapper,
 } from './UserForm.styled';
 
 export const UserForm = () => {
-    const user = useSelector(selectUser);
-    console.log('user', user);
-    const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  console.log('user', user);
+  const dispatch = useDispatch();
 
-    // const [avatarUrl, setAvatarUrl] = useState('');
-    const [newBirthday, setNewBirthday] = useState('');
-    // const [startDate, setStartDate] = useState(null);
+  // const [avatarUrl, setAvatarUrl] = useState('');
+  const [newBirthday, setNewBirthday] = useState('');
+  // const [startDate, setStartDate] = useState(null);
 
+  // const [startDate, setStartDate] = useState(new Date(user?.birthday));
+  const [startDate, setStartDate] = useState(new Date());
 
-    // const [startDate, setStartDate] = useState(new Date(user?.birthday));
-    const [startDate, setStartDate] = useState(new Date());
+  const handleFormSubmit = async (values, { resetForm }) => {
+    const formData = new FormData();
+    console.log(startDate);
+    try {
+      if (initialValues.name !== values.name && values.name) {
+        formData.append('name', values.name);
+      }
+      if (initialValues.email !== values.email && values.email) {
+        formData.append('email', values.email);
+      }
+      if (initialValues.phone !== values.phone) {
+        formData.append('phone', values.phone);
+      }
+      if (initialValues.skype !== values.skype) {
+        formData.append('skype', values.skype);
+      }
+      formData.append(
+        'birthday',
+        new Date(startDate).toLocaleDateString('en-GB')
+      );
 
+      for (const value of formData.values()) {
+        console.log(value);
+      }
+      await dispatch(updateUser(formData));
+      resetForm();
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
+  const initialValues = {
+    avatarURL: user?.avatarURL,
+    name: user?.name || '',
+    email: user?.email || '',
+    phone: user?.phone || '',
+    skype: user?.skype || '',
+    // birthday: newBirthday || user?.birthday
+    //     ? (user?.birthday || newBirthday)
+    //     : "",
+    // birthday: user?.birthday || "",
+    // new Date().toLocaleDateString('en-GB'),
+  };
+  console.log(initialValues);
+  return (
+    <Wrapper>
+      <Formik
+        initialValues={initialValues}
+        enableReinitialize={true}
+        onSubmit={handleFormSubmit}
+      >
+        {({ values, handleSubmit, handleChange, handleBlur, dirty }) => (
+          <div>
+            <FormUserProfile autoComplete="off" onSubmit={handleSubmit}>
+              <LabelAvatar htmlFor="avatarURL">
+                <AvatarWrapper>
+                  <Avatar src={user.avatarUrl} alt="avatar" />
+                  <BtnUploadAvatar
+                    type="file"
+                    name="avatarURL"
+                    accept=".jpg, .jpeg, .png"
+                  />
+                </AvatarWrapper>
+              </LabelAvatar>
+              <UserName>
+                {values.name ? values.name : initialValues.name}
+              </UserName>
+              <UserLabel>User</UserLabel>
 
-    const handleFormSubmit = async (values, { resetForm }) => {
-        const formData = new FormData();
-        console.log(startDate);
-        try {
-            if (initialValues.name !== values.name && values.name) {
-                formData.append('name', values.name);
-            }
-            if (initialValues.email !== values.email && values.email) {
-                formData.append('email', values.email);
-            }
-            if (initialValues.phone !== values.phone) {
-                formData.append('phone', values.phone);
-            }
-            if (initialValues.skype !== values.skype) {
-                formData.append('skype', values.skype);
-            }
-            formData.append('birthday', new Date(startDate).toLocaleDateString('en-GB'));
+              <MainFieldWrapper>
+                <BlockFieldWrapper>
+                  <Label htmlFor="name">
+                    User Name
+                    <Input
+                      type="text"
+                      name="name"
+                      value={values.name}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      placeholder="Your Name"
+                    />
+                  </Label>
+                  <DatePickerWrapper>
+                    <Label htmlFor="birthday">
+                      Birthday
+                      <DatePickerStyles
+                        showYearDropdown
+                        dirty
+                        type={'date'}
+                        input={true}
+                        selected={startDate}
+                        onChange={date => {
+                          setStartDate(date);
+                          setNewBirthday(date.toLocaleDateString('en-GB'));
+                          console.log(newBirthday);
+                        }}
+                        minDate={new Date('1923-01-01T00:00:00')}
+                        maxDate={new Date()}
+                        formatWeekDay={nameOfDay => nameOfDay.slice(0, 1)}
+                        calendarStartDay={1}
+                        placeholderText="Click to select a date"
+                        // value={values.birthday}
+                      />
+                    </Label>
+                  </DatePickerWrapper>
+                  <Label htmlFor="email">
+                    Email
+                    <Input type="email" name="email" />
+                  </Label>
+                </BlockFieldWrapper>
+                <BlockFieldWrapper>
+                  <Label htmlFor="phone">
+                    Phone
+                    <Input type="text" name="phone" />
+                  </Label>
 
-            for (const value of formData.values()) {
-                console.log(value);
-            }
-            await dispatch(updateUser(formData));
-            resetForm();
-        }
-        catch (e) {
-            console.error(e);
-        }
-    };
+                  <Label htmlFor="skype">
+                    Skype
+                    <Input
+                      type="text"
+                      name="skype"
+                      value={values.skype ? values.skype : ''}
+                      placeholder="Add a skype number"
+                    />
+                  </Label>
+                </BlockFieldWrapper>
+              </MainFieldWrapper>
 
-
-
-
-    const initialValues = {
-        avatarURL: user?.avatarURL,
-        name: user?.name || '',
-        email: user?.email || '',
-        phone: user?.phone || '',
-        skype: user?.skype || '',
-        // birthday: newBirthday || user?.birthday
-        //     ? (user?.birthday || newBirthday)
-        //     : "",
-        // birthday: user?.birthday || "",
-        // new Date().toLocaleDateString('en-GB'),
-
-
-    };
-    console.log(initialValues);
-    return (
-        <Wrapper>
-            <Formik
-                initialValues={initialValues}
-                enableReinitialize={true}
-                onSubmit={handleFormSubmit}>
-                {({
-                    values,
-                    handleSubmit,
-                    handleChange,
-                    handleBlur,
-                    dirty,
-                }) =>
-                (
-                    < div >
-                        <FormUserProfile autoComplete='off' onSubmit={handleSubmit} >
-                            <LabelAvatar htmlFor="avatarURL">
-                                <AvatarWrapper>
-                                    <Avatar src={user.avatarUrl} alt="avatar" />
-                                    <BtnUploadAvatar type="file" name="avatarURL" accept=".jpg, .jpeg, .png" />
-                                </AvatarWrapper>
-                            </LabelAvatar>
-                            <UserName>{values.name ? values.name : initialValues.name}</UserName>
-                            <UserLabel>User</UserLabel>
-
-                            <MainFieldWrapper>
-                                <BlockFieldWrapper>
-                                    <Label htmlFor="name">
-                                        User Name
-                                        <Input
-                                            type="text"
-                                            name="name"
-                                            value={values.name}
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                            placeholder="Your Name" />
-                                    </Label>
-                                    <DatePickerWrapper>
-                                        <Label htmlFor="birthday">
-                                            Birthday
-                                            <DatePickerStyles
-                                                showYearDropdown
-                                                dirty
-                                                type={'date'}
-                                                input={true}
-                                                selected={startDate}
-                                                onChange={(date) => { setStartDate(date); setNewBirthday(date.toLocaleDateString('en-GB')); console.log(newBirthday) }}
-                                                minDate={new Date('1923-01-01T00:00:00')}
-                                                maxDate={new Date()}
-                                                formatWeekDay={nameOfDay => nameOfDay.slice(0, 1)}
-                                                calendarStartDay={1}
-                                                placeholderText="Click to select a date"
-                                            // value={values.birthday}
-                                            />
-
-                                        </Label>
-                                    </DatePickerWrapper>
-                                    <Label htmlFor="email">
-                                        Email
-                                        <Input type="email" name="email" />
-                                    </Label>
-                                </BlockFieldWrapper>
-                                <BlockFieldWrapper>
-                                    <Label htmlFor="phone">
-                                        Phone
-                                        <Input type="text" name="phone" />
-                                    </Label>
-
-                                    <Label htmlFor="skype">
-                                        Skype
-                                        <Input
-                                            type="text"
-                                            name="skype"
-                                            value={values.skype ? values.skype : ''}
-                                            placeholder="Add a skype number" />
-                                    </Label>
-                                </BlockFieldWrapper>
-                            </MainFieldWrapper>
-
-                            <SubmitBtn type="submit" disabled={!dirty}>Save changes</SubmitBtn>
-                        </FormUserProfile>
-                    </div>
-                )
-                }
-            </Formik>
-
-
-        </Wrapper >
-    );
+              <SubmitBtn type="submit" disabled={!dirty}>
+                Save changes
+              </SubmitBtn>
+            </FormUserProfile>
+          </div>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 };
-
