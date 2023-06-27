f0df9d593ddfa589737e7175462b48395a27d15b
Anatolij Goga <anzefal@gmail.com>
2023-06-27 18:32:10 +0300

    Закончил с редаксом для обзоров
    Закончил писать reviews\operations.js и reviews\reviewsSlice.js
    Нужен тест

--- src/redux/reviews/operations.js
+++ src/redux/reviews/operations.js
@@ -1,89 +1,80 @@
-// import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// const instance = axios.create({
-//   baseURL: 'https://bra1n-gain-backend.onrender.com/api',
-// });
+const instance = axios.create({
+  baseURL: 'https://bra1n-gain-backend.onrender.com/api',
+});
 
-// export const fetchAllReviews = createAsyncThunk(
-//   'reviews/fetchAllReviews',
-//   async (_, thunkAPI) => {
-//     try {
-//       const response = await instance.get('/reviews?page=1&limit=10');
+export const fetchAllReviews = createAsyncThunk(
+  'reviews/fetchAllReviews',
+  async (_, thunkAPI) => {
+    try {
+      const response = await instance.get('/reviews/');
 
-//       return response.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
-// export const fetchUserReviews = createAsyncThunk(
-//   'reviews/fetchUserReviews',
-//   async (_, thunkAPI) => {
-//     try {
-//       const response = await instance.get('/reviews/my-reviews?limit=30');
+export const fetchUserReviews = createAsyncThunk(
+  'reviews/fetchUserReviews',
+  async ({ _id: owner }, thunkAPI) => {
+    try {
+      const response = await instance.get('/reviews/own', { _id: owner });
 
-//       return response.data.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
+      return response.data.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
-// export const fetchUserReviewById = createAsyncThunk(
-//   'reviews/fetchUserReviewById',
-//   async (id, thunkAPI) => {
-//     try {
-//       const response = await instance.get(`/reviews/my-reviews/${id}`);
+export const addReview = createAsyncThunk(
+  'reviews/addReview',
+  async ({ _id: owner, name, avatarUrl, rating, comment }, thunkAPI) => {
+    try {
+      const response = await instance.post('/reviews/own', {
+        _id: owner,
+        name,
+        avatarUrl,
+        rating,
+        comment,
+      });
 
-//       return response.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
+      return response.data.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
-// export const addReview = createAsyncThunk(
-//   'reviews/addReview',
-//   async ({ rating, comment }, thunkAPI) => {
-//     try {
-//       const response = await instance.post('/reviews/my-reviews', {
-//         rating,
-//         comment,
-//       });
+export const updateReview = createAsyncThunk(
+  'reviews/updateReview',
+  async ({ _id: owner, rating, comment }, thunkAPI) => {
+    try {
+      const response = await instance.put(`/reviews/own`, {
+        _id: owner,
+        rating,
+        comment,
+      });
 
-//       return response.data.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
-// export const updateReview = createAsyncThunk(
-//   'reviews/updateReviews',
-//   async ({ id, rating, comment }, thunkAPI) => {
-//     try {
-//       const response = await instance.put(`/reviews/my-reviews/${id}`, {
-//         rating,
-//         comment,
-//       });
+export const deleteReview = createAsyncThunk(
+  'reviews/deleteReview',
+  async ({ _id: owner }, thunkAPI) => {
+    try {
+      const response = await instance.delete(`/reviews/own`, { _id: owner });
 
-//       return response.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-// export const deleteReview = createAsyncThunk(
-//   'reviews/deleteReview',
-//   async (id, thunkAPI) => {
-//     try {
-//       const response = await instance.delete(`/reviews/my-reviews/${id}`);
-
-//       return response.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
---
+++ src/redux/reviews/reviewsSlice.js
@@ -0,0 +1,5 @@
+export const selectAllReviews = state => state.reviews.reviews;
+
+export const selectReviewsError = state => state.reviews.error;
+
+export const selectReviewsLoading = state => state.reviews.isLoading;
