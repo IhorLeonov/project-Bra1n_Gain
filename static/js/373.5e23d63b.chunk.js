4cc439a61cb38377cbfd6475905b321f45115c6a
IhorLeonov <ihorleonow@gmail.com>
2023-07-01 20:31:32 +0000

    Deploying to gh-pages from @ IhorLeonov/project-Bra1n_Gain@3e114662aafb87cb3c7dfd9811f5e56fcfb9e96d 🚀

---
+++ static/js/373.5e23d63b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoosetrack=self.webpackChunkgoosetrack||[]).push([[373],{4373:function(t,a,e){e.d(a,{QAE:function(){return r}});var n=e(9983);function r(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M405 136.798L375.202 107 256 226.202 136.798 107 107 136.798 226.202 256 107 375.202 136.798 405 256 285.798 375.202 405 405 375.202 285.798 256z"}}]})(t)}}}]);
+//# sourceMappingURL=373.5e23d63b.chunk.js.map
