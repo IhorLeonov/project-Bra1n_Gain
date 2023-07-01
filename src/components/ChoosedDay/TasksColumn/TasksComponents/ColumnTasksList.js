d16b683a70740eb97abf9a4c2eedd70a1723d6dc
Ihor Leonov <ihorleonow@gmail.com>
2023-07-01 14:25:00 +0300

    commented nanoid

--- src/components/ChoosedDay/TasksColumn/TasksComponents/ColumnTasksList.js
+++ src/components/ChoosedDay/TasksColumn/TasksComponents/ColumnTasksList.js
@@ -1,17 +1,30 @@
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 import { List, ListItem } from './ColumnTasksList.styled';
 import { TaskColumnCard } from './TaskColumnCard/TaskColumnCard';
 
-
-export const ColumnTasksList = ({ listId, setTargetElement, setTaskModalOpen}) => {
-
-  const test = ["Erruer esse enim cillum voluptate minim voluptate velit eiusmod dolor Lorem.", "Euer esse enim cillum voluptate minim voluptate velit eiusmod dolor Lorem.", "Eue esse enim cillum voluptate minim voluptate velit eiusmod dolor Lorem.", "Eu esse enim cillum voluptate minim voluptate velit eiusmod dolor Lorem."]
+export const ColumnTasksList = ({
+  listId,
+  setTargetElement,
+  setTaskModalOpen,
+}) => {
+  const test = [
+    'Erruer esse enim cillum voluptate minim voluptate velit eiusmod dolor Lorem.',
+    'Euer esse enim cillum voluptate minim voluptate velit eiusmod dolor Lorem.',
+    'Eue esse enim cillum voluptate minim voluptate velit eiusmod dolor Lorem.',
+    'Eu esse enim cillum voluptate minim voluptate velit eiusmod dolor Lorem.',
+  ];
   return (
     <List>
-      {test.map( e => (<ListItem key={e}>
-        <TaskColumnCard listId={listId} task={e} setTaskModalOpen={setTaskModalOpen} setTargetElement={setTargetElement}/>
-      </ListItem>))}
-
+      {test.map(e => (
+        <ListItem key={e}>
+          <TaskColumnCard
+            listId={listId}
+            task={e}
+            setTaskModalOpen={setTaskModalOpen}
+            setTargetElement={setTargetElement}
+          />
+        </ListItem>
+      ))}
     </List>
   );
 };
