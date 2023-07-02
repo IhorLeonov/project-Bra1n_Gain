8f72aa5a2e4bf18aaf0c04b5225ad93b0abe91d5
Ihor Leonov <ihorleonow@gmail.com>
2023-07-02 12:06:35 +0300

    Update AddFeedbackModal.js

--- src/components/AddFeedbackModal/AddFeedbackModal.js
+++ src/components/AddFeedbackModal/AddFeedbackModal.js
@@ -12,30 +12,30 @@ export const AddFeedbackModal = () => {
 
   useEffect(() => {
     getReview();
-  }, []);
+  }, []); // eslint-disable-line
 
   const startRating = () => {
     if (feedback) {
-      return feedback.rate
+      return feedback.rate;
     } else {
-      return 0
+      return 0;
     }
-  }
+  };
 
   const startComment = () => {
     if (feedback) {
-      return feedback.comment
+      return feedback.comment;
     } else {
-      return 0
+      return 0;
     }
-  }
+  };
 
   const getReview = async () => {
     try {
       const response = await axios.get(
         'https://bra1n-gain-backend.onrender.com/api/reviews/own'
       );
-      console.log(response.data)
+      console.log(response.data);
       setFeedback(response.data);
       console.log(feedback);
     } catch (error) {
@@ -49,7 +49,10 @@ export const AddFeedbackModal = () => {
   return (
     <Modal>
       {modalTypeSelected === 'feedback' && (
-        <FeedbackForm startRating={startRating()} startComment={startComment()}></FeedbackForm>
+        <FeedbackForm
+          startRating={startRating()}
+          startComment={startComment()}
+        ></FeedbackForm>
       )}
     </Modal>
   );
