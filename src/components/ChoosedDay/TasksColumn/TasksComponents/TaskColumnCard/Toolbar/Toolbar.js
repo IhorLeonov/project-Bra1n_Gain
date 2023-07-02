666ee6c4e0e74454e0aeeb36bf2239f0206a6fb5
Pumpon4yk <tt10392@gmail.com>
2023-07-02 19:27:01 +0200

    add delete task

--- src/components/ChoosedDay/TasksColumn/TasksComponents/TaskColumnCard/Toolbar/Toolbar.js
+++ src/components/ChoosedDay/TasksColumn/TasksComponents/TaskColumnCard/Toolbar/Toolbar.js
@@ -1,9 +1,13 @@
 import { SectionToolbar, TaskAction, Icon } from './Toolbar.styled';
 import { useDispatch } from 'react-redux';
 import { setModalAction, toggleModal, setDataTask, setModalTypeTask } from 'redux/modal/modalSlice';
+import { deleteTask } from 'redux/task/operations';
 
 export const Toolbar = ({ setTargetElement, setTaskModalOpen, task }) => {
   const dispatch = useDispatch();
+
+  
+const {_id } = task;
 
   const handleClick = e => {
     setTaskModalOpen(true);
@@ -15,6 +19,11 @@ export const Toolbar = ({ setTargetElement, setTaskModalOpen, task }) => {
     dispatch(setModalAction("edit"));
     dispatch(setModalTypeTask());
     dispatch(setDataTask(task));
+  };
+
+  const handleDeleteTask = e => {
+    dispatch(deleteTask(_id))
+
   };
 
   return (
@@ -34,7 +43,7 @@ export const Toolbar = ({ setTargetElement, setTaskModalOpen, task }) => {
           alt="button-edit"
         />
       </TaskAction>
-      <TaskAction>
+      <TaskAction onClick={handleDeleteTask}>
         <Icon
           src={process.env.PUBLIC_URL + '/images/icons/icon-trash-16x16.svg'}
           alt="button-delete"
