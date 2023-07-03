b213559506b28d19e0c31b282b8bf841573b3bb6
Pumpon4yk <tt10392@gmail.com>
2023-07-03 05:26:08 +0200

    make one func to 2 btn add task

--- src/components/ChoosedDay/TasksColumn/TasksColumn.js
+++ src/components/ChoosedDay/TasksColumn/TasksColumn.js
@@ -24,7 +24,7 @@ export const TasksColumn = ({ listId, tasks }) => {
 
   return (
     <ColumnsItem>
-      <ColumnHeadBar title={listId} />
+      <ColumnHeadBar title={listId} handleAddTask={handleAddTaskButtonClick}/>
 
       <ColumnTasksList
         listId={listId}
