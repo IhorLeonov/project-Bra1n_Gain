93e086627e0cc65d72e733166da95d63ac51439f
Ihor Leonov <ihorleonow@gmail.com>
2023-07-04 15:02:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
 import { Routes, Route } from 'react-router-dom';
 import { SharedLayout } from 'components/SharedLayout/SharedLayout';
-import { lazy, useEffect } from 'react';
+import { lazy, useEffect, ComponentType } from 'react';
 import { useDispatch } from 'react-redux';
 import { refreshUser } from 'redux/auth/operations';
 import { useAuth } from 'hooks/useAuth';
 import { Loader } from 'components/Loader/Loader';
 import { PrivateRoute } from 'routes/PrivateRoute';
 import { RestrictedRoute } from 'routes/RestrictedRoute';
+
+// const lazyRetry = function (componentImport) {
+//   return new Promise((resolve, reject) => {
+//     // check if the window has already been refreshed
+//     const hasRefreshed = JSON.parse(
+//       window.sessionStorage.getItem('retry-lazy-refreshed') || 'false'
+//     );
+//     // try to import the component
+//     componentImport()
+//       .then(component => {
+//         window.sessionStorage.setItem('retry-lazy-refreshed', 'false'); // success so reset the refresh
+//         resolve(component);
+//       })
+//       .catch(error => {
+//         if (!hasRefreshed) {
+//           // not been refreshed yet
+//           window.sessionStorage.setItem('retry-lazy-refreshed', 'true'); // we are now going to refresh
+//           return window.location.reload(); // refresh the page
+//         }
+//         reject(error); // Default error behaviour as already tried refresh
+//       });
+//   });
+// };
+
+// const YourPage = lazy(() =>
+//   lazyRetry(() => import('./pages/YourPage'), 'YourPage')
+// );
 
 const Register = lazy(() => import('pages/Register/Register'));
 const Login = lazy(() => import('pages/Login/Login'));
@@ -17,6 +44,7 @@ const MainLayout = lazy(() => import('pages/MainLayout/MainLayout'));
 const StatisticsPage = lazy(() =>
   import('pages/StatisticsPage/StatisticsPage')
 );
+
 const ChoosedMonth = lazy(() => import('components/ChoosedMonth/ChoosedMonth'));
 const ChoosedDay = lazy(() => import('components/ChoosedDay/ChoosedDay'));
 const NotFound = lazy(() => import('pages/NotFound/NotFound'));
