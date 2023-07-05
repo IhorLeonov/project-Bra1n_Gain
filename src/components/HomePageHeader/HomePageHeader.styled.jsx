7bf97b96f8ef28ce3a79256645c50ad7fccfd041
Ihor Leonov <ihorleonow@gmail.com>
2023-07-05 17:14:22 +0300

    Update HomePageHeader.styled.jsx

--- src/components/HomePageHeader/HomePageHeader.styled.jsx
+++ src/components/HomePageHeader/HomePageHeader.styled.jsx
@@ -1,6 +1,19 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: ${props => props.theme.colors.primaryBlueColor};
+  height: 812px;
+
+  @media (min-width: 768px) {
+    height: 100vh;
+  }
+`;
+
 export const LogoImg = styled.img`
   width: 142px;
   height: 142px;
@@ -12,28 +25,10 @@ export const LogoImg = styled.img`
     height: 150px;
     margin-bottom: 0;
   }
-  &:hover, &:focus {
+  &:hover,
+  &:focus {
     transform: scale(1.3);
     transition-duration: 500ms;
-  }
-`;
-
-export const Wrapper = styled.div`
-  padding-top: 232px;
-  background-color: ${props => props.theme.colors.primaryBlueColor};
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-bottom: 88px;
-
-  @media (min-width: 768px) {
-    padding-top: 321px;
-  }
-
-  @media (min-width: 1280px) {
-    padding-top: 187px;
-    padding-bottom: 200px;
   }
 `;
 
@@ -44,7 +39,6 @@ export const MainTitle = styled.h1`
   font-family: var(--title-font);
   font-weight: 600;
 
-  
   @media (min-width: 768px) {
     font-size: 120px;
     margin-bottom: 40px;
@@ -54,7 +48,7 @@ export const AcсentTitle = styled.span`
   font-style: italic;
 `;
 export const BtnContainer = styled.div`
-display: flex;
+  display: flex;
   flex-direction: column-reverse;
 
   @media (min-width: 768px) {
@@ -64,11 +58,11 @@ display: flex;
   }
 `;
 
-
 export const SignUp = styled(NavLink)`
   display: flex;
   justify-content: center;
   padding: 15.5px 24px;
+  margin-top: 208px;
 
   color: #fff;
   font-family: var(--main-font);
@@ -77,13 +71,15 @@ export const SignUp = styled(NavLink)`
   line-height: 14px;
   font-weight: 600;
 
-  &:hover, &:focus {
+  &:hover,
+  &:focus {
     transform: scale(1.2);
     transition-duration: 500ms;
   }
 
   @media (min-width: 768px) {
-  line-height: 18px;
+    margin-top: 0; /////
+    line-height: 18px;
     font-size: 14px;
     width: 121px;
     height: 49px;
@@ -95,13 +91,12 @@ export const Login = styled(NavLink)`
   justify-content: center;
   align-items: center;
   gap: 6px;
-  margin-bottom: 221px;
-  
+
   width: 131px;
   height: 46px;
   border-radius: 15px;
   color: #3e85f3;
-  
+
   font-family: var(--main-font);
   font-weight: 600;
   font-size: 14px;
@@ -120,4 +115,3 @@ export const Login = styled(NavLink)`
     margin-bottom: 0;
   }
 `;
-
