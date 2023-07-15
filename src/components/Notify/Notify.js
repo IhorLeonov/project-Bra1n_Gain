7491a839a18e713e223fdebbc1378234d33cbc4d
Ihor Leonov <ihorleonow@gmail.com>
2023-07-15 23:34:20 +0300

    Update Notify.js

--- src/components/Notify/Notify.js
+++ src/components/Notify/Notify.js
@@ -4,12 +4,19 @@ export const ToasterNotify = message => {
   switch (message) {
     case 'AccountСreated':
       return toast.success(
-        'Account created. To confirm your registration, please follow the link that was sent to your email address.'
+        'Account created. To confirm your registration, please follow the link that was sent to your email address.',
+        {
+          duration: 3000,
+        }
       );
     case 'LoginSuccessful':
-      return toast.success('login successfully.');
+      return toast.success('login successfully.', {
+        duration: 3000,
+      });
     case 'RegisterSuccessful':
-      return toast.success('Registration successfully complited');
+      return toast.success('Registration successfully complited', {
+        duration: 3000,
+      });
 
     default:
       return toast.error(message);
