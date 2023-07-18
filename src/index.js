7293b78cab919b84c17f883e4dd417bd095abbeb
pumpon <pumpon@debian-BULLSEYE-live-builder-AMD64>
2023-07-18 23:50:25 +0200

    get back strick mod

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename="/project-Bra1n_Gain">
@@ -18,5 +18,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </BrowserRouter>
       </PersistGate>
     </Provider>
-  //</React.StrictMode>
+  </React.StrictMode>
 );
